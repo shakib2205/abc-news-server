f0ec56e593046e4752e689c4a1affd8411fcf325
shakib2205 <shakibur35-2737@diu.edu.bd>
2023-07-13 02:14:56 +0600

    added more function

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const express = require ('express')
+const express = require('express')
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-const categories = require('./data/categories.json'); 
+const categories = require('./data/categories.json');
+const news = require('./data/news.json');
 
 app.get('/', (req, res) => {
     res.send('News api running')
@@ -13,8 +14,30 @@ app.get('/', (req, res) => {
 
 app.get('/news-categories', (req, res) => {
     res.send(categories);
+});
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(news);
+    }
+    else {
+        const category_news = news.filter(n => n.category_id === id);
+        res.send(category_news);
+    }
+
+});
+
+app.get('/news', (req, res) => {
+    res.send(news);
+} )
+
+app.get('/news/:id', (req, res) => { 
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews);
 })
 
-app.listen(port, ()=> {
+app.listen(port, () => {
     console.log('abc News Server Running on port', port);
 });
